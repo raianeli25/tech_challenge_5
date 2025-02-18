ceec51a7c4abdf404819babe27a04624990d4845
Chrystian Remes <chrystian@windowslive.com>
2025-02-17 22:25:30 -0300

    Improved Documentation of model_funcs.py and added __init__.py file

--- recommendation/utils/model_funcs.py
+++ recommendation/utils/model_funcs.py
@@ -9,7 +9,6 @@ from lightfm import LightFM
 
 from utils.custom_data_structs import UserItemData
 
-
 SEED = 42
 K_LIGHTFM_ITEMS = 6
 K_POPULAR_ITEMS = 6
@@ -20,90 +19,142 @@ random.seed(SEED)
 loaded_user_item_data:UserItemData = pickle.load(open('artifacts/user_item_data.pkl', 'rb'))
 loaded_n_users, loaded_n_items = loaded_user_item_data.interactions_shape
 
+
 def get_user_item_data() -> UserItemData:
-      return loaded_user_item_data
+    """
+    Simple get function to return "loaded_user_item_data", 
+    loaded from pickle file.
+    """  
+    return loaded_user_item_data
 
 
 
-def format_newuser_input(user_feature_map, user_feature_list):
-	normalised_val = 1.0 
-	target_indices = []
-	for feature in user_feature_list:
-		try:
-				target_indices.append(user_feature_map[feature])
-		except KeyError:
-				print("new user feature encountered '{}'".format(feature))
-				pass
-	#print("target indices: {}".format(target_indices))
-	new_user_features = np.zeros(len(user_feature_map.keys()))
-	for i in target_indices:
-		new_user_features[i] = normalised_val
-	new_user_features = sparse.csr_matrix(new_user_features)
-	return(new_user_features)
+def format_newuser_input(user_feature_map:dict, user_feature_list:list)->sparse.csr_matrix:
+    """
+    This function receives a 'user_feature_list' and its mapping 'user_feature_map'.
+    It is used to create a sparse matrix representation for user_feature presence.
+    """
+    normalised_val = 1.0
+    target_indices = []
+    for feature in user_feature_list:
+        # try to make the mapping from user_feature_list -> indexes
+        try:
+            target_indices.append(user_feature_map[feature])
+        # if it does not exist, just pass (ignore)
+        except KeyError:
+            print("new user feature encountered '{}'".format(feature))
+            pass
+    # initialize a vector [0 0 0 0 0 ... 0]
+    new_user_features = np.zeros(len(user_feature_map.keys()))
+    # for the found indexes above, change the entry to normalised_val (1.0)
+    for i in target_indices:
+        new_user_features[i] = normalised_val
+    # transform into sparse matrix and return
+    new_user_features = sparse.csr_matrix(new_user_features)
+    return(new_user_features)
+
+
 
 def get_scores_from_model(user_hash:str,user_feature_list:list|str,user_item_data:UserItemData,model:LightFM):
     """
-    This function 
+    This function receives a user_hash and generates a full array of scores for recommendation.
+    The scores array have size equal to `loaded_n_items`, i.e., the number of items used for training.
     """
+
+    # in case of one entry 'string', it should be converted to ['string']
     if isinstance(user_feature_list, str):
         user_feature_list = [user_feature_list]
+    # try to recommend for known users
     try:
         user_x = user_item_data.user_id_map[user_hash]
-        scores = model.predict(user_x, np.arange(loaded_n_items)) # means predict for all
+        scores = model.predict(user_x, np.arange(loaded_n_items))
+    # recommend for new/unknown user
     except:
         new_user_features = format_newuser_input(user_item_data.user_feature_map, user_feature_list)
         scores = model.predict(0, np.arange(loaded_n_items), user_features=new_user_features)
     return scores
 
+
+
 def get_top_items_by_model_scores(scores, user_item_data):
     """
-    This function verifies if the users is known or new, and makes recommendations depending on this verification.
     The top K_LIGHTFM_ITEMS recommendations from the list are returned.
     """
     
     top_k_indices = np.argsort(-scores)[:K_LIGHTFM_ITEMS]  # Sort scores in descending order and take the top K_ITEMS
     return [user_item_data.item_id_map_reverse[i] for i in top_k_indices]
 
+
+
 def recommend_by_model_scores(user_hash:str,user_feature_list:list|str,user_item_data:UserItemData,model:LightFM):
     """
-    This function 
+    This function is just a wrapper to call both get_scores_from_model()
+    and get_top_items_by_model_scores() in sequence.
     """
     
     scores = get_scores_from_model(user_hash,user_feature_list,user_item_data,model)
     return get_top_items_by_model_scores(scores, user_item_data)
 
-def count_histories_by_popularity(df:pd.DataFrame):
+
+
+def count_histories_by_popularity(df:pd.DataFrame, item_column:str="history")->pd.DataFrame:
     """
-    This function 
+    This function receives a dataframe containing the item_column
+    and returns the number of occurences of such item in the dataset.
+    Also, the counts are ordered descending (greater first).
     """
     
-    return df.groupby(["history"]).size().sort_values(ascending=False).astype(dtype="UInt16")
+    return df.groupby([item_column]).size().sort_values(ascending=False).astype(dtype="UInt16")
 
-def get_df_most_popular_histories(df_count_histories:pd.DataFrame):
-    return df_count_histories.iloc[:K_POPULAR_ITEMS]
 
-def get_df_most_unpopular_histories(df_count_histories:pd.DataFrame):
-    return df_count_histories.iloc[K_POPULAR_ITEMS:]
 
-def recommend_by_most_popular(df:pd.DataFrame):
+def get_df_most_popular_histories(df_count_histories:pd.DataFrame, k_pop_items:int=K_POPULAR_ITEMS)->pd.DataFrame:
     """
-    This function 
+    Considering an ordered (descending) dataset, 
+    recommends the k_pop_items rows with most counts.
+    """
+    return df_count_histories.iloc[:k_pop_items]
+
+
+
+def get_df_most_unpopular_histories(df_count_histories:pd.DataFrame, k_pop_items:int=K_POPULAR_ITEMS)->pd.DataFrame:
+    """
+    Considering an ordered (descending) dataset, 
+    recommends all the remaining rows from df_count_histories, except the
+    first k_pop_items rows.
+    """
+    return df_count_histories.iloc[k_pop_items:]
+
+
+
+def recommend_by_most_popular(df:pd.DataFrame)->list:
+    """
+    This function is a wrapper to get most popular items.
     """
     df_count_histories = count_histories_by_popularity(df)
     df_popular_stories = get_df_most_popular_histories(df_count_histories)
     return list(set(df_popular_stories.keys()))
 
-def recommend_by_weighted_random(df_unpopular_histories:pd.DataFrame, unpopular_weights:list):
+
+
+def recommend_by_weighted_random(df_unpopular_histories:pd.DataFrame, unpopular_weights:list, k_sample_items:int=K_SAMPLED_ITEMS)->list:
     """
-    This function 
+    This function will select randomly `k_sample_items` from `df_unpopular_histories`.
+    The probability of an item to be selected is proportional to `unpopular_weights`.
+    Thus, most popular items have more probability to be selected (by not guarantees -> random!)
     """
-    random_k_histories = df_unpopular_histories.sample(n=K_SAMPLED_ITEMS, weights=unpopular_weights, random_state=random.randint(0, 50))
+    random_k_histories = df_unpopular_histories.sample(n=k_sample_items, weights=unpopular_weights, random_state=random.randint(0, 50))
     # return random_k_histories
     return list(set(random_k_histories.keys()))
 
-def order_df_by_most_recent(df:pd.DataFrame, history_list:list):
-    return df[df["history"].isin(history_list)].sort_values("age_exp_normalized",ascending=False)
 
-    
-def list_intersection(list1:list, list2:list):
+
+def list_intersection(list1:list, list2:list)->list:
+    """
+    Given lists `list1` and `list2`, return the intersection of their items.
+    Example: 
+    list1 = ['a', 'b', 'c', 'd']
+    list1 = ['c', 'd', 'e']
+    Then, the return is ['c', 'd']
+    """
     return list(set(list1) & set(list2))
